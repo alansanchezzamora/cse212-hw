69f30334d24faf2ae100f0f1195b703c815e33ce
Alan Sánchez <alan.sanchez.zamor@gmail.com>
2025-09-06 21:52:00 -0600

    w1 hw

--- week01/analyze/Sorting.cs
+++ week01/analyze/Sorting.cs
@@ -1,17 +1,28 @@
-﻿public static class Sorting {
-    public static void Run() {
-        var numbers = new[] { 3, 2, 1, 6, 4, 9, 8 };
+﻿public static class Sorting
+{
+    public static void Run()
+    {
+        var numbers = new[] { 3, 2, 1, 6, 4, 9, 8, 11 };
         SortArray(numbers);
         Console.Out.WriteLine("int[]{{{0}}}", string.Join(", ", numbers)); //int[]{1, 2, 3, 4, 6, 8, 9}
     }
 
-    private static void SortArray(int[] data) {
-        for (var sortPos = data.Length - 1; sortPos >= 0; sortPos--) {
-            for (var swapPos = 0; swapPos < sortPos; ++swapPos) {
-                if (data[swapPos] > data[swapPos + 1]) {
+    private static void SortArray(int[] data)
+    {
+        var vueltas = 0;
+
+        for (var sortPos = data.Length - 1; sortPos >= 0; sortPos--)
+        {
+            for (var swapPos = 0; swapPos < sortPos; ++swapPos)
+            {
+                vueltas++;
+                if (data[swapPos] > data[swapPos + 1])
+                {
                     (data[swapPos + 1], data[swapPos]) = (data[swapPos], data[swapPos + 1]);
                 }
             }
         }
+        Console.WriteLine($"vueltas : {vueltas}");
+
     }
 }
--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,23 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        ///
+        /// Plan : 
+        /// Create an array named multiples 
+        /// Make a for loop with parameters of i=1; i == lenght; i++
+        /// Multiply the "number" * i 
+        /// append the result to the multiples array
+        ///
+
+        ///int[] multiples = new int[length];
+        List<double> multiples = new();
+        for (int i = 1; i <= length; i++)
+        {
+            double value = i * number;
+            multiples.Add(value);
+        }
+
+        return multiples.ToArray(); ; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +45,14 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        ///
+        /// tienes que contar el tamano de la lista y tener el numero, despues restarle donde quieren comenzar, 9 - 3 = 6 
+        /// y luego en otra lista ir por el valor 6 de la lista original y sumarle 1 hasta llegar al 9
+        /// en otra lista ir de la posicion 1 hasta la 6 con los valores de la lista original y depues pegar las dos listas. 
+        /// 
+        /// 
+        
+        
     }
 }
